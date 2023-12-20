f9bc01d1b0cfe29f58cfc57083d53dcd9eb8b589
srujan986921@gmail.com <srujan986921@gmail.com>
2023-12-20 13:45:23 +0000

    updates

--- models/example/medicare_highest_cost_illness.sql
+++ models/example/medicare_highest_cost_illness.sql
@@ -1,28 +1,28 @@
-SELECT
+select
     drg_definition,
     total_discharges,
     provider_name,
     cast(provider_id as string) as provider_id,
     average_medicare_payments
-FROM
+from
     (
-        SELECT
+        select
             drg_definition,
             total_discharges,
             provider_name,
             cast(provider_id as string) as provider_id,
             average_medicare_payments
-        FROM `bigquery-public-data.medicare.inpatient_charges_2012`
+        from `bigquery-public-data.medicare.inpatient_charges_2012`
 
-        UNION ALL
+        union all
 
-        SELECT
+        select
             drg_definition,
             total_discharges,
             provider_name,
             provider_id,
             average_medicare_payments
-        FROM `bigquery-public-data.cms_medicare.inpatient_charges_2012`
+        from `bigquery-public-data.cms_medicare.inpatient_charges_2012`
     ) combined_tables
-ORDER BY average_medicare_payments DESC
-LIMIT 10;
+order by average_medicare_payments desc
+limit 10
--- models/example/medicare_highest_discharges.sql
+++ models/example/medicare_highest_discharges.sql
@@ -2,13 +2,8 @@
     materialized="table"
 ) }}
 
-with highest_discharges as (
-    select
-        *,
-        row_number() over (order by total_discharges desc) as row_num
-    from `bigquery-public-data.cms_medicare.inpatient_charges_2012`
-)
-
-select *
-from highest_discharges
-where row_num = 1;
+select *,
+       row_number() over (order by total_discharges desc) as row_num
+from `bigquery-public-data.cms_medicare.inpatient_charges_2012`
+order by total_discharges desc
+limit 1
